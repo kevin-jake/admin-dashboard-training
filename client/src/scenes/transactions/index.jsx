0056973f03d0c2f44c82ae2e23058639cac9e354
Kevin Jake <kevin_jake_04@yahoo.com>
2023-01-19 12:27:04 +0800

    add transaction page server side datagrid

---
+++ client/src/scenes/transactions/index.jsx
@@ -0,0 +1,112 @@
+import { Box, useTheme } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import DataGridCustomToolbar from "components/DataGridCustomToolbar";
+import Header from "components/Header";
+import React, { useState } from "react";
+import { useGetTransactionsQuery } from "state/api";
+
+const columns = [
+  {
+    field: "_id",
+    headerName: "ID",
+    flex: 1,
+  },
+  {
+    field: "userId",
+    headerName: "User ID",
+    flex: 1,
+  },
+  {
+    field: "createdAt",
+    headerName: "CreatedAt",
+    flex: 1,
+  },
+  {
+    field: "products",
+    headerName: "# of Products",
+    flex: 0.5,
+    sortable: false,
+    renderCell: (params) => params.value.length,
+  },
+  {
+    field: "cost",
+    headerName: "Cost",
+    flex: 1,
+    renderCell: (params) => `Php ${Number(params.value).toFixed(2)}`,
+  },
+];
+
+const Transactions = () => {
+  const theme = useTheme();
+
+  const [page, setPage] = useState(0);
+  const [pageSize, setPageSize] = useState(20);
+  const [sort, setSort] = useState({});
+  const [search, setSearch] = useState("");
+
+  const [searchInput, setSearchInput] = useState("");
+  const { data, isLoading } = useGetTransactionsQuery({
+    page,
+    pageSize,
+    sort: JSON.stringify(sort),
+    search,
+  });
+  console.log("🚀 ~ file: index.jsx:13 ~ Transactions ~ data", data);
+
+  return (
+    <Box m="1.5rem 2.5rem">
+      <Header title="TRANSACTIONS" subtitle="List of Transactions" />
+      <Box
+        mt="40px"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundCotor: theme.palette.primary.light,
+          },
+          "& .MuiDataGrid-footerContainer": {
+            backgroundCotor: theme.palette.background.ait,
+            color: theme.palette.secondary[100],
+            borderTop: "none",
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `${theme.palette.secondary[200]} !important`,
+          },
+        }}
+      >
+        <DataGrid
+          loading={isLoading || !data}
+          getRowId={(row) => row._id}
+          rows={(data && data.transactions) || []}
+          columns={columns}
+          rowCount={(data && data.total) || 0}
+          rowsPerPageOptions={[20, 50, 100]}
+          pagination
+          page={page}
+          pageSize={pageSize}
+          paginationMode="server"
+          sortingMode="server"
+          onPageChange={(newPage) => setPage(newPage)}
+          onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+          onSortModelChange={(newSortModel) => setSort(...newSortModel)}
+          components={{ Toolbar: DataGridCustomToolbar }}
+          componentsProps={{
+            toolbar: { searchInput, setSearchInput, setSearch },
+          }}
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default Transactions;
--- server/index.js
+++ server/index.js
@@ -13,8 +13,14 @@ import managementRoutes from "./routes/management.js";
 // data imports
 import User from "./models/User.js";
 import Product from "./models/Product.js";
-import { dataUser, dataProduct, dataProductStat } from "./data/index.js";
 import ProductStat from "./models/ProductStat.js";
+import Transaction from "./models/Transaction.js";
+import {
+  dataUser,
+  dataProduct,
+  dataProductStat,
+  dataTransaction,
+} from "./data/index.js";
 
 // CONFIGURATION
 dotenv.config();
@@ -49,5 +55,6 @@ mongoose
     // User.insertMany(dataUser);
     // Product.insertMany(dataProduct);
     // ProductStat.insertMany(dataProductStat);
+    // Transaction.insertMany(dataTransaction);
   })
   .catch((error) => console.log(`${error} did not connect`));
